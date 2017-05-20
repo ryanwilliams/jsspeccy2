3890e2564544f57bcc1570dbd2ba7ca9d05e795e
Ryan Williams <ryan@ryanwillia.ms>
2017-05-20 12:15:57 +1200

    require using rom loader directly

--- src/autoloaders.js
+++ src/autoloaders.js
@@ -1,4 +1,4 @@
 export default {
-  'tape_48.z80': require('../autoloaders/tape_48.z80'),
-  'tape_128.z80': require('../autoloaders/tape_128.z80'),
+  'tape_48.z80': require('../rom-loader!../autoloaders/tape_48.z80'),
+  'tape_128.z80': require('../rom-loader!../autoloaders/tape_128.z80'),
 };
--- src/roms.js
+++ src/roms.js
@@ -1,6 +1,6 @@
 export default {
-  '48.rom': require('../roms/48.rom'),
-  '128-0.rom': require('../roms/128-0.rom'),
-  '128-1.rom': require('../roms/128-1.rom'),
+  '48.rom': require('../rom-loader!../roms/48.rom'),
+  '128-0.rom': require('../rom-loader!../roms/128-0.rom'),
+  '128-1.rom': require('../rom-loader!../roms/128-1.rom'),
 };
 
